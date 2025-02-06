d203c5a4b7280f1dd3f474f42bbcdb0345bd8cf9
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-02-06 12:10:47 +0100

    modif jenkins

--- src/main/java/com/genio/GenioServiceApplication.java
+++ src/main/java/com/genio/GenioServiceApplication.java
@@ -2,19 +2,34 @@ package com.genio;
 
 
 import com.genio.service.impl.ModeleService;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
+import java.net.URL;
 
 @SpringBootApplication
 public class GenioServiceApplication implements CommandLineRunner {
 
     @Autowired
     private ModeleService modeleService;
+    private static final Logger logger = LogManager.getLogger(GenioServiceApplication.class);
+
 
     public static void main(String[] args) {
+        URL log4jConfigFile = GenioServiceApplication.class.getClassLoader().getResource("log4j2.xml");
+        if (log4jConfigFile == null) {
+            System.out.println("Fichier log4j2.xml introuvable !");
+        } else {
+            System.out.println("log4j2.xml trouvé à : " + log4jConfigFile);
+            Configurator.initialize(null, log4jConfigFile.getPath());
+        }
+
+        logger.info("Application démarrée avec Log4J2 !");
         SpringApplication.run(GenioServiceApplication.class, args);
     }
 
