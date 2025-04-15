b40f0a9b5a76c6a19e81957b281048b8d7cc5a5d
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-04-15 15:15:40 +0200

    Ajout de test 11.2

--- src/main/java/com/genio/dto/StageDTO.java
+++ src/main/java/com/genio/dto/StageDTO.java
@@ -99,4 +99,10 @@ public class StageDTO {
                 ", anneeStage='" + anneeStage + '\'' +
                 '}';
     }
+
+    public StageDTO(String anneeStage, String sujetDuStage, String dateDebutStage, String dateFinStage,
+                    String duree, int joursTot, int heuresTot, String remunerationHoraire, String saeStageProfessionnel) {
+        this(anneeStage, sujetDuStage, dateDebutStage, dateFinStage, duree,
+                Integer.valueOf(joursTot), Integer.valueOf(heuresTot), remunerationHoraire, saeStageProfessionnel);
+    }
 }
