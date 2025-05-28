56fed918ea14f8faaefb004f9bd1d023a341ef1a
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-05-28 19:23:05 +0200

    ajout header copyright

--- src/main/java/com/genio/mapper/DocxParser.java
+++ src/main/java/com/genio/mapper/DocxParser.java
@@ -1,3 +1,17 @@
+/*
+ *  GenioService
+ *  ------------
+ *  Copyright (c) 2025
+ *  Elsa HADJADJ <elsa.simha.hadjadj@gmail.com>
+ *
+ *  Licence sous Creative Commons CC-BY-NC-SA 4.0.
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  https://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ *  Dépôt GitHub (Back) :
+ *  https://github.com/DUT-Info-Montreuil/GenioService
+ */
+
 package com.genio.mapper;
 
 import org.springframework.web.multipart.MultipartFile;
