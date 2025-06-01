6ea70565590b8a226464461d06f8fe1f105e6ddd
elsouche123 <elsa.simha.hadjadj@gmail.com>
2025-06-01 14:21:45 +0200

    correction sonarqube

--- src/test/java/com/genio/dto/UtilisateurUpdateDTOTest.java
+++ src/test/java/com/genio/dto/UtilisateurUpdateDTOTest.java
@@ -1,17 +1,3 @@
-/*
- *  GenioService
- *  ------------
- *  Copyright (c) 2025
- *  Elsa HADJADJ <elsa.simha.hadjadj@gmail.com>
- *
- *  Licence sous Creative Commons CC-BY-NC-SA 4.0.
- *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
- *  https://creativecommons.org/licenses/by-nc-sa/4.0/
- *
- *  Dépôt GitHub (Back) :
- *  https://github.com/DUT-Info-Montreuil/GenioService
- */
-
 package com.genio.dto;
 
 import org.junit.jupiter.api.Test;
@@ -133,6 +119,7 @@ class UtilisateurUpdateDTOTest {
         dto.setRole("ROLE");
         dto.setActif(true);
 
+        assertNotNull(dto);
         assertNotEquals(null, dto);
 
         assertNotEquals(dto, new Object());
